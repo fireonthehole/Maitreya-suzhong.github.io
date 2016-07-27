070a090676bbf65ed91ce53cf4f53b9bcbe641c7
Maitreya-suzhong <chiweixi@icloud.com>
2016-07-27 14:32:39 -0700

    new content

---
+++ e/data/adminlogin/user1_cccdfee00ad704a4320f931bc86e5b4c.php
@@ -0,0 +1,12 @@
+<?php
+$ecms_adminloginr=array();
+$ecms_adminloginr=Array('userid'=>'1',
+'ernd'=>'jji4idrYU5btW9Cz0kWG5H8MFax',
+'erndtwo'=>'4yNSqtlbKteNXkLB3k2J',
+'erndadd'=>'DTe5sAnicdhKQIagX9GNFmkcX6cr4t6I',
+'ehash'=>'snMS0RLJgByPSG4wjhZh',
+'ehashname'=>'ehash_TdgW',
+'rhash'=>'R1M90tzt6hN7',
+'rhashname'=>'rhash_VA0J',
+'edbdata'=>'1');
+?>
